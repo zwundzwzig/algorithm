767fd3e6572d299fd1e0e8db23ab283ec29686ee
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-12-26 15:29:23 +0900

    polynomial

---
+++ src/level_zero/polynomial.js
@@ -0,0 +1,29 @@
+// 한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다.
+// 다항식을 계산할 때는 동류항끼리 계산해 정리합니다.
+// 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때,
+// 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요.
+// 같은 식이라면 가장 짧은 수식을 return 합니다.
+
+function polynomial(polynomial) {
+  let infrontX = 0;
+  let integer = 0;
+  let answer = "";
+
+  for (let i of polynomial.split(" + ")) {
+    if (i === "x") i = "1x";
+    if (i.includes("x")) infrontX += +i.substring(0, i.length - 1);
+    else integer += +i;
+  }
+
+  return infrontX === 0
+      ? (answer += integer)
+    : integer === 0
+      ? infrontX === 1
+        ? (answer += "x")
+        : (answer += infrontX + "x")
+    : infrontX === 1
+      ? (answer += "x + " + integer)
+    : (answer += infrontX + "x + " + integer);
+}
+
+export default { polynomial };
