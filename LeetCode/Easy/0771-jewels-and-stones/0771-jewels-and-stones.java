0bdcd826eddd25739f0d59d13a68817cdc30e46f
zwundzwzig <104782275+zwundzwzig@users.noreply.github.com>
2024-01-05 09:47:08 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (5.92%) - LeetHub

---
+++ LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewels.indexOf(stones.charAt(i)) >= 0) 
+                count++;
+        }
+        
+        return count;
+    }
+}
