eb42729e4fcebed9030d8f8fc43842a69e650e58
zwundzwzig <104782275+zwundzwzig@users.noreply.github.com>
2024-01-16 22:41:52 +0900

    Time: 34 ms (90.91%), Space: 18.6 MB (31.34%) - LeetHub

---
+++ baekjun/오육삼오.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class 오육삼오 {
+
+  private static String youngest = "";
+  private static String oldest = "";
+  private static int youngestYear = Integer.MIN_VALUE;
+  private static int oldestYear = Integer.MAX_VALUE;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < n; i++) {
+      String[] input = br.readLine().split(" ");
+      String name = input[0];
+      int day = Integer.parseInt(input[1]);
+      int month = Integer.parseInt(input[2]);
+      int year = Integer.parseInt(input[3]);
+
+      int birthYear = year * 10000 + month * 100 + day;
+
+      if (birthYear < oldestYear) {
+        oldestYear = birthYear;
+        oldest = name;
+      }
+
+      if (birthYear > youngestYear) {
+        youngestYear = birthYear;
+        youngest = name;
+      }
+    }
+
+    System.out.println(youngest);
+    System.out.println(oldest);
+
+  }
+
+}
