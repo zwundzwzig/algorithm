b2335cc902a552b79c0a7f742b3f46830a6632a8
zwundzwzig <amoeba8282@gmail.com>
2024-02-14 20:12:12 +0900

    2024-02-14

--- baekjun/입출력.java
+++ baekjun/입출력.java
@@ -108,6 +108,88 @@ public class 입출력 {
     br.close();
   }
 
+  public static void 일일공이이(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int size = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < size; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      sb.append("Case #" + (i + 1) + ": " + a + " + " + b + " = ").append(a + b).append("\n");
+    }
+
+    System.out.println(sb);
+    br.close();
+  }
+
+  public static void 일일칠일팔(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    String s;
+
+    while ((s = br.readLine()) != null) {
+      sb.append(s).append("\n");
+    }
+
+    System.out.println(sb);
+    br.close();
+  }
+
+  public static void 일일칠이일(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String word = br.readLine();
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+
+    for (int i = 0; i < word.length(); i++) {
+      sb.append(word.charAt(i));
+      count++;
+
+      if (count == 10) {
+        sb.append("\n");
+        count = 0;
+      }
+    }
+
+    System.out.println(sb);
+    br.close();
+  }
+
+  public static void 이칠사일(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String word = br.readLine();
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+
+    for (int i = 0; i < word.length(); i++) {
+      sb.append(word.charAt(i));
+      count++;
+
+      if (count == 10) {
+        sb.append("\n");
+        count = 0;
+      }
+    }
+
+    System.out.println(sb);
+    br.close();
+  }
+
+  public static void 이칠삼구(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int target = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= 9; i++) {
+      sb.append(target + " * " + i + " = " + (target * i)).append("\n");
+    }
+
+    System.out.println(sb);
+    br.close();
+  }
+
   @Test
   public void 정답_일공공공() throws IOException {
     String input = "1 2";
