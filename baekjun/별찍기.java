691c0e3c67dc1878bc0a216d99b1e5c9bb7bf3a9
zwundzwzig <amoeba8282@gmail.com>
2024-02-21 16:00:37 +0900

    2024-02-21

--- baekjun/별찍기.java
+++ baekjun/별찍기.java
@@ -5,6 +5,13 @@ import java.io.*;
 import java.util.Scanner;
 
 public class 별찍기 {
+  /*
+   *
+   **
+   ***
+   ****
+   *****
+  */
   public static void 이사삼팔(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
@@ -17,6 +24,13 @@ public class 별찍기 {
     }
   }
 
+  /*
+       *
+      **
+     ***
+    ****
+   *****
+   */
   public static void 이사삼구(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -36,6 +50,13 @@ public class 별찍기 {
     bw.close();
   }
 
+  /*
+   *****
+   ****
+   ***
+   **
+   *
+   */
   public static void 이사사공(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
@@ -49,6 +70,13 @@ public class 별찍기 {
     System.out.print(sb);
   }
 
+  /*
+  *****
+   ****
+    ***
+     **
+      *
+  */
   public static void 이사사일(String[] args) {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
@@ -65,6 +93,13 @@ public class 별찍기 {
     }
   }
 
+  /*
+       *
+      ***
+     *****
+    *******
+   *********
+  */
   public static void 이사사이(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
@@ -86,6 +121,14 @@ public class 별찍기 {
     br.close();
   }
 
+
+  /*
+   *********
+    *******
+     *****
+      ***
+       *
+  * */
   public static void 이사사삼(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
@@ -105,6 +148,239 @@ public class 별찍기 {
 
     System.out.print(sb);
     br.close();
+  }
+
+  /*
+       *
+      ***
+     *****
+    *******
+   *********
+    *******
+     *****
+      ***
+       *
+  * */
+  public static void 이사사사(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    br.close();
+
+    // 상단
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n - i; j++) {
+        sb.append(" ");
+      }
+
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        sb.append("*");
+      }
+
+      sb.append("\n");
+    }
+
+    // 하단
+    for (int i = n - 1; i >= 1; i--) {
+      for (int j = 1; j <= n - i; j++) {
+        sb.append(" ");
+      }
+
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        sb.append("*");
+      }
+
+      sb.append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  /*
+   *        *
+   **      **
+   ***    ***
+   ****  ****
+   **********
+   ****  ****
+   ***    ***
+   **      **
+   *        *
+   * */
+  public static void 이사사오(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    br.close();
+
+    // 상단 부분 출력
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= i; j++) {
+        sb.append("*");
+      }
+      for (int j = 1; j <= 2 * (N - i); j++) {
+        sb.append(" ");
+      }
+      for (int j = 1; j <= i; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+
+    // 하단 부분 출력
+    for (int i = N - 1; i >= 1; i--) {
+      for (int j = 1; j <= i; j++) {
+        sb.append("*");
+      }
+      for (int j = 1; j <= 2 * (N - i); j++) {
+        sb.append(" ");
+      }
+      for (int j = 1; j <= i; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  /*
+   *********
+    *******
+     *****
+      ***
+       *
+      ***
+     *****
+    *******
+   *********
+   * */
+  public static void 이사사육(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    br.close();
+
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < i; j++) {
+        sb.append(" ");
+      }
+
+      for (int j = (2*n) - (i*2-1); j > 0; j--) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n-i; j++) {
+        sb.append(" ");
+      }
+
+      for (int j = 1; j <= 2*i-1; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  /*
+     *
+    **
+   ***
+    **
+     *
+  */
+  public static void 이오이이(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int size = Integer.parseInt(br.readLine());
+    br.close();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= size; i++) {
+      for (int j = size - i; j > 0; j--) {
+        sb.append(" ");
+      }
+      for (int j = 1; j <= i; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+
+    for (int i = 1; i <= size - 1; i++) {
+      for (int j = 1; j <= i; j++) {
+        sb.append(" ");
+      }
+      for (int j = 1; j <= size - i; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+    System.out.print(sb);
+  }
+
+  /*
+      *
+     * *
+    * * *
+   * * * *
+  */
+  public static void 일공구구일(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int size = Integer.parseInt(br.readLine());
+    br.close();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= size; i++) {
+      for (int j = 1; j <= size - i; j++) {
+        sb.append(" ");
+      }
+
+      for (int j = 1; j <= i*2-1; j++) {
+        if (j % 2 == 0) sb.append(" ");
+        else sb.append("*");
+      }
+      sb.append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  /*
+      *
+     * *
+    *   *
+   *******
+ */
+  public static void 일공구구이(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int size = Integer.parseInt(br.readLine());
+    br.close();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= size; i++) {
+      for (int j = 1; j <= size - i; j++) {
+        sb.append(" ");
+      }
+
+      if (i == 1 || i == size) {
+        for (int j = 1; j <= 2 * i - 1; j++) {
+          sb.append("*");
+        }
+      }
+      else {
+        sb.append("*");
+        for (int j = 2; j < 2 * i - 1; j++) {
+          sb.append(" ");
+        }
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+
+    System.out.println(sb);
   }
 
   @Test
@@ -143,5 +419,106 @@ public class 별찍기 {
     Assertions.assertEquals("  ***", result[2]);
     Assertions.assertEquals(" ****", result[3]);
     Assertions.assertEquals("*****", result[4]);
+  }
+
+  @Test
+  public void 정답_이사사사() throws IOException {
+    String input = "5";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+
+    이사사사(new String[]{input});
+
+    String[] result = outContent.toString().split(System.lineSeparator());
+    Assertions.assertEquals("    *", result[0]);
+    Assertions.assertEquals("   ***", result[1]);
+    Assertions.assertEquals("  *****", result[2]);
+    Assertions.assertEquals(" *******", result[3]);
+    Assertions.assertEquals("*********", result[4]);
+    Assertions.assertEquals(" *******", result[5]);
+    Assertions.assertEquals("  *****", result[6]);
+    Assertions.assertEquals("   ***", result[7]);
+    Assertions.assertEquals("    *", result[8]);
+  }
+
+  @Test
+  public void 정답_이사사육() throws IOException {
+    String input = "5";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+
+    이사사육(new String[]{input});
+
+    String[] result = outContent.toString().split(System.lineSeparator());
+    Assertions.assertEquals("*********", result[0]);
+    Assertions.assertEquals(" *******", result[1]);
+    Assertions.assertEquals("  *****", result[2]);
+    Assertions.assertEquals("   ***", result[3]);
+    Assertions.assertEquals("    *", result[4]);
+    Assertions.assertEquals("   ***", result[5]);
+    Assertions.assertEquals("  *****", result[6]);
+    Assertions.assertEquals(" *******", result[7]);
+    Assertions.assertEquals("*********", result[8]);
+  }
+
+  @Test
+  public void 정답_이오이이() throws IOException {
+    String input = "3";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+
+    이오이이(new String[]{input});
+
+    String[] result = outContent.toString().split(System.lineSeparator());
+    Assertions.assertEquals("  *", result[0]);
+    Assertions.assertEquals(" **", result[1]);
+    Assertions.assertEquals("***", result[2]);
+    Assertions.assertEquals(" **", result[3]);
+    Assertions.assertEquals("  *", result[4]);
+  }
+
+  @Test
+  public void 정답_일공구구일() throws IOException {
+    String input = "4";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+
+    일공구구일(new String[]{input});
+
+    String[] result = outContent.toString().split(System.lineSeparator());
+    Assertions.assertEquals("   *", result[0]);
+    Assertions.assertEquals("  * *", result[1]);
+    Assertions.assertEquals(" * * *", result[2]);
+    Assertions.assertEquals("* * * *", result[3]);
+  }
+
+  @Test
+  public void 정답_일공구구이() throws IOException {
+    String input = "4";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+
+    일공구구이(new String[]{input});
+
+    String[] result = outContent.toString().split(System.lineSeparator());
+    Assertions.assertEquals("   *", result[0]);
+    Assertions.assertEquals("  * *", result[1]);
+    Assertions.assertEquals(" *   *", result[2]);
+    Assertions.assertEquals("*******", result[3]);
   }
 }
